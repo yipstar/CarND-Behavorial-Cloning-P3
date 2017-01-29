a90a5c2a4f5ae48fafb34d9cef48b0f061e6134a
Gianni Jacklone <gianni.jacklone@gmail.com>
2017-01-28 17:23:43 -0800

    Use a validation set.

--- model3.py
+++ model3.py
@@ -25,11 +25,12 @@ if refine:
         model.load_weights("model.h5")
 
 else:
-    data_dir = "udacity_data"
-    driving_log = pd.read_csv(data_dir + "/driving_log.csv")
+    driving_log = pd.read_csv("udacity_data/driving_log.csv")
+    validation_driving_log = pd.read_csv("training_data/driving_log.csv")
 
     learning_rate = 0.001
     samples_per_epoch = 20000
+    nb_val_samples = 2000
     p_threshold = 1
     num_epochs = 10
 
@@ -40,7 +41,7 @@ model.compile(optimizer=adam, loss="mse")
 
 batch_size = 64
 
-run_version = 20
+run_version = 21
 
 dir = "working_model_version_" + str(run_version)
 if not os.path.exists(dir):
@@ -51,8 +52,9 @@ from generator2 import data_generator
 for epoch_index in range(num_epochs):
 
     datagen = data_generator(driving_log, batch_size, p_threshold)
+    validation_datagen = data_generator(validation_driving_log, batch_size, p_threshold)
 
-    model.fit_generator(datagen, samples_per_epoch=samples_per_epoch, nb_epoch=1, verbose=1, )
+    model.fit_generator(datagen, samples_per_epoch=samples_per_epoch, nb_epoch=1, verbose=1, validation_data=validation_datagen, nb_val_samples=nb_val_samples)
 
     # increase the probability of dropping small angle data on each iteration
     p_threshold = 1 / (epoch_index + 1) * 1
